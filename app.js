819da6dc87102e11652a6423bccd4b69cb1f66b1
Abbosbek <zloy19781999@gmail.com>
2022-12-22 19:08:22 +0500

    new_version

--- app.js
+++ app.js
@@ -1,10 +1,35 @@
-var elIMG = document.querySelector(".site-nav-img")
-var elLink = document.querySelectorAll(".site-nav-link")
 
+var elNavItems = document.querySelectorAll(".sitenav__item");
+var elNavLinks = document.querySelectorAll(".sitenav__link");
 
+elNavLinks.forEach((link) => {
+    link.addEventListener("click", function(){
+        elNavItems.forEach(function(item){
+            item.classList.remove("sitenav__item--active");
+        })
 
-elLink.forEach((el) => {
-    el.addEventListener("click", () => {
-        elIMG.style.display = "none"
+        link.parentElement.classList.add("sitenav__item--active");
     })
 })
+
+
+var elBtn = document.querySelector(".end-wrapper__request-link");
+var elCloseBtn = document.querySelector(".modal-close");
+var elModal = document.querySelector(".modal");
+
+
+elBtn.addEventListener("click", function() {
+    elModal.classList.add("modal-open");
+    // elModal.style.display = "flex";
+});
+
+elCloseBtn.addEventListener("click", function() {
+    elModal.classList.remove("modal-open");
+});
+
+window.onclick = function(e) {
+    // console.log(e.target)
+    if(e.target == elModal){
+        elModal.classList.remove("modal-open");
+    }
+};
